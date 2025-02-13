49dd44958033c792bde931dd4875646b5ce33c99
hyeyoung <roenv9@gmail.com>
2025-02-13 19:01:41 +0900

    Add files via upload

--- codeup/CodeUp1461.java
+++ codeup/CodeUp1461.java
@@ -1,10 +1,25 @@
 package codeup;
 
+import java.util.Scanner;
+
 public class CodeUp1461 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		
+		int[][] arr = new int[size][size];
+		
+		int num = 1;
+		
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr[i].length; j++){
+			arr[i][j] = (num*size)*(i+1)-j;
+			System.out.printf("%d ", (num*size)*(i+1)-j);
+			}
+			System.out.println();
+		}
+		sc.close();
 	}
 
 }
